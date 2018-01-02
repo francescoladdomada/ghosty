0c492b06223ace389eedbab2bad956a3bb1d1d6f
Francesco Laddomada <francesco.laddomada@gmail.com>
2018-01-02 21:25:45 +0100

    multitouch controls

--- Assets/Scripts/Gameplay/TouchControls.cs
+++ Assets/Scripts/Gameplay/TouchControls.cs
@@ -9,27 +9,43 @@ public class TouchControls : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
 
 	public int stepQuantity;
 	private bool pressed = false;
+	private Vector2 pointerDownPosition;
+	private float deadZone = 75f;
 
 	void Start() {
 		_player = Object.FindObjectOfType<PlayerController>();
+		pointerDownPosition = Vector2.zero;
 	}
 
 	void Update() {
+
 		if (!pressed)
 			return;
-		MakeStep ();
+		//MakeStep (1);
 	}
 
+	/*
 	public void MakeStep() {
 		_player.MakeStep (stepQuantity);
+		pressed = false;
+	}
+	*/
+	public void MakeStep(int quantity) {
+		_player.MakeStep (quantity);
 		pressed = false;
 	}
 
 	public void OnPointerDown(PointerEventData eventData) {
 		pressed = true;
+		pointerDownPosition = eventData.pressPosition;
 	}
 		
 	public void OnPointerUp(PointerEventData eventData) {
+		if (eventData.position.y - deadZone > pointerDownPosition.y) {
+			MakeStep (2);
+		} else {
+			MakeStep (1);
+		}
 		pressed = false;
 	}
 }
